de2100e69d2af13079ca2e29244cc91ad2620c64
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-12-09 13:10:04 +0300

    passing tests 2-4

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+
+function submitData(name, email){
+    const userInfo = {
+        name: name,
+        email: email,
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(userInfo),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+
+        const display = document.getElementById('display');
+        const responseId = document.createElement('h1');
+
+        responseId.textContent = data.id;
+        display.appendChild(responseId);
+    })
+
+    .catch((object) => {
+        
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = object.message;
+
+        const display = document.getElementById('display');
+        display.appendChild(errorMessage);
+    })
+}
